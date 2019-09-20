73a099131069ba09c6f298e1a6b0662c4ac9254b
Jack Tsang <cic_tian2@yahoo.com>
2019-09-19 19:12:48 -0700

    Added sport.sh bash script file

---
+++ sport.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/ bash
+#This script takes two parameters and concatenates them into a useful message.
+
+NAME=$1
+FAVORITE_SPORT=$2
+
+echo My name is $NAME and my favorite sport is $FAVORITE_SPORT.
