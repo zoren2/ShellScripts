a1afa1662eac130222fcbfd1c8d12b5e4e4772b7
Jack Tsang <cic_tian2@yahoo.com>
2019-09-19 19:09:47 -0700

    Added greetings.sh script file

---
+++ greetings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin bash
+#This shell script interpolates variables into simple echo commands.
+
+NAME="Jack T"
+FAVORITE_COLOR=purple
+
+echo Hi $NAME, your favorite color is $FAVORITE_COLOR.
