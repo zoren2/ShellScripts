a9151f1cf2bde0a60967463051a4a9c5a86366dd
Jack Tsang <cic_tian2@yahoo.com>
2019-09-19 20:28:17 -0700

    Added pipe.sh bash script file

---
+++ pipe.sh
@@ -0,0 +1,13 @@
+#!/usr/bin bash
+
+#LS with one column, reverse sort, top three lines of text
+FILES=`ls -1 | sort -r | head -3`
+COUNT=1
+
+for FILE in $FILES
+do
+	echo "File #$COUNT = $FILE"
+	((COUNT++))
+done
+
+exit 0
