0aeb4356637ba907e71b245f24506f81a5915805
Jack Tsang <cic_tian2@yahoo.com>
2019-09-19 19:29:41 -0700

    Added env.sh bash script file

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/ bash
+#This script prints out a users computer name, user name, and home directory.
+
+echo "This is your computer name, username, and home directory."
+echo "The computer's name is $HOSTNAME"
+echo "The username is $USER"
+echo "Your home path is: $HOME"
