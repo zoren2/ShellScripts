1bb3415e5eeb2a822b4c33c945ab297146b5ceb3
Jack Tsang <cic_tian2@yahoo.com>
2019-09-20 20:08:42 -0700

    Added user.sh bash script file

---
+++ user.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/ bash
+#This script takes user input after running the script and returns the user name
+#and age.
+
+VALID=0
+
+while [ $VALID -eq 0 ]
+do
+	read -p "Please enter you rname and age: " NAME AGE
+	if [[ ( -z $NAME ) || ( -z $AGE ) ]]
+	then
+		echo "Not enough parameters passed"
+		continue
+	elif [[ ! $NAME =~ ^[A-Za-z]+$ ]] # =~ matches the regular expression
+	then
+		echo "Non alpha characters detected [$NAME]"
+		continue
+	elif [[ ! $AGE =~ ^[0-9]+$ ]]
+	then
+		echo "Non digit characters detected [$AGE]"
+		continue
+	fi
+	VALID=1
+done
+echo "Name = $NAME, Age = $AGE"
+exit 0
